9fb1d48af896455d70709f2798f7aa3d0d54d881
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-11-28 11:08:37 +0100

    Add docstring to measure_runtime function for clarity

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+"""Measure the runtime of multiple async comprehensions
+running concurrently."""
 
 import asyncio
 import time
@@ -6,6 +8,8 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
+    """Measures the total runtime of running four async comprehensions
+    concurrently."""
     start = time.time()
     await asyncio.gather(*(async_comprehension() for i in range(4)))
     end = time.time()
