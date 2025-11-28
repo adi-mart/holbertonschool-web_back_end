011d6e50ab808b4ab799b6f62d2f009b2987b508
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-11-28 10:35:57 +0100

    Fix indentation in docstring of async_generator function

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -7,7 +7,7 @@ from typing import Generator
 
 
 async def async_generator() -> Generator[float, None, None]:
-  """Async generator that yields random numbers."""
+    """Async generator that yields random numbers."""
     for i in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
