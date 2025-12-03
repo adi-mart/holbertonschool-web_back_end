29dfc6ae4fb6efa6e73048c02ba1beade200b750
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-12-03 15:30:24 +0100

    Add module docstring for listing all documents in MongoDB collection

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+""" Module for listing all documents in a MongoDB collection. """
+
+
 def list_all(mongo_collection):
     """ list all documents in a collection """
     return mongo_collection.find()
