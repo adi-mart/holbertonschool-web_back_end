2ecc8979373d5e8f66a53544bb47c890d902d669
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-12-03 15:32:11 +0100

    Add missing module docstring for insert_school function

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+""" Module for inserting a new document in a MongoDB collection. """
+
+
 def insert_school(mongo_collection, **kwargs):
     """ insert a new document in a collection """
     return mongo_collection.insert_one(kwargs).inserted_id
