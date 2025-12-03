d803b273677afa4966391d62b3528e7fd34cf92d
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-12-04 00:13:02 +0100

    Fix update_topics function to use update_many for updating multiple documents

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -4,7 +4,7 @@
 
 def update_topics(mongo_collection, name, topics):
     """ update topics for a school document in a collection """
-    mongo_collection.update_one(
+    mongo_collection.update_many(
         {"name": name},
         {"$set": {"topics": topics}}
     )
