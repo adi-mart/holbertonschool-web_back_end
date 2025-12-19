d0ee8b980ab071b47ab89711069652a1f2e2645d
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-12-19 14:11:05 +0100

    fix: correct casing of 'firstName' and 'lastName' in handleProfileSignup function

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -4,7 +4,7 @@ export default function handleProfileSignup() {
 	return Promise.all([uploadPhoto(), createUser()])
 		.then((value) => {
 			const [photo, user] = value;
-			console.log(`${photo.body} ${user.firstname} ${user.lastname}`);
+			console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
 		})
 		.catch(() => {
 			console.log('Signup system offline');
