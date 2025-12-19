2d90e96423e68cbcc27bbe9e3aa27770f193e3db
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-12-19 16:13:26 +0100

    feat: enhance handleProfileSignup to map results with status and value

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -6,4 +6,10 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
 		signUpUser(firstName, lastName),
 		uploadPhoto(fileName)
 	])
+  .then((results) => results.map((result) => {
+    if (result.status === 'fulfilled') {
+      return { status: result.status, value: result.value };
+    }
+    return { status: result.status, value: result.reason };
+  }));
 }
