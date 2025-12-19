59142e3300d14526ac94f13d76ecf1abe433f7f6
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-12-19 16:22:37 +0100

    fix: correct uploadPhoto import path in handleProfileSignup function

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,12 +1,12 @@
 import signUpUser from "./4-user-promise";
-import { uploadPhoto } from "./utils";
+import uploadPhoto from "./5-photo-reject.js";
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
 	return Promise.allSettled([
 		signUpUser(firstName, lastName),
 		uploadPhoto(fileName)
 	])
-  .then((results) => results.map((result) => ({
+	.then((results) => results.map((result) => ({
     	status: result.status, 
 			value: result.status === 'fulfilled' ? result.value : result.reason
 		})));
