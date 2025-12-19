c1f3f845577691d1e82b35e5697ef07bfd3939b0
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-12-19 14:07:59 +0100

    fix: add missing semicolons for consistency in handleProfileSignup function

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -3,8 +3,8 @@ import { createUser, uploadPhoto } from './utils'
 export default function handleProfileSignup() {
 	return Promise.all([uploadPhoto(), createUser()])
 		.then((value) => {
-			const [photo, user] = value
-			console.log(`${photo.body} ${user.firstname} ${user.lastname}`)
+			const [photo, user] = value;
+			console.log(`${photo.body} ${user.firstname} ${user.lastname}`);
 		})
 		.catch(() => {
 			console.log('Signup system offline');
