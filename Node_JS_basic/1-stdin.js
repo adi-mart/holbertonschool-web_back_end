aa5134ee2c725a59889ce4e07630d209929459ca
adi-mart <aurelie.di-martino@holbertonstudents.com>
2026-01-07 18:26:31 +0100

    fix: simplify name input handling in stdin interaction

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,10 +1,7 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.setEncoding('utf8');
-
-process.stdin.on('data', (chunk) => {
-  const name = chunk.trim();
-  process.stdout.write(`Your name is: ${name}\n`);
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data}`);
 });
 
 process.stdin.on('end', () => {
