63f28beaef8729c1ec479f2c0fd26ffb6b11197f
adi-mart <aurelie.di-martino@holbertonstudents.com>
2026-01-08 21:18:00 +0100

    feat: implement Express server with student data retrieval functionality

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', (req, res) => {
+  let output = '';
+  const originalConsoleLog = console.log;
+  console.log = (msg) => { output += msg + '\n'; };
+  countStudents(process.argv[2]).then(() => {
+    console.log = originalConsoleLog;
+    res.send('This is the list of our students\n' + output.trim());
+  }).catch((error) => {
+    console.log = originalConsoleLog;
+    res.send(error.message);
+  });
+});
+
+app.listen(1245);
+
+module.exports = app;
