323a6b4991200b539c3bd786d70da1d79b8c21ce
adi-mart <aurelie.di-martino@holbertonstudents.com>
2026-01-09 23:53:04 +0100

    feat: update routes import to use consistent naming convention

--- Node_JS_basic/full_server/server.js
+++ Node_JS_basic/full_server/server.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import routes from './routes/index';
+import router from './routes/index';
 
 const app = express();
-app.use('/', routes);
+
+app.use(router);
 
 app.listen(1245);
 
